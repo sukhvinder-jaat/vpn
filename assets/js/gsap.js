c0ce2d0653b59bd6ff3eed71131b49c4defbce82
sukhvinder-jaat <monudhaka829@gmail.com>
2023-11-04 11:52:37 +0530

    sdfgh

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,10 +1,18 @@
 gsap.from("#show li,#btns li, .logo", {
-  y: 50,
+  // y: 50,
   opacity: 0,
-  duration: 0.5,
+  // duration: 0.5,
   delay: 0.5,
   stagger: 0.5,
+  duration: 0.5,
+  // ease: "bounce(0.5)",
+  // ease: "myWiggle",
+  y: 100,
+  x: 100,
+  // wiggles: 10,
+  // type: "anticipate",
 });
+// hero
 gsap.from(".heroPara #heroParaAnimation", {
   x: -50,
   opacity: 0,
@@ -18,15 +26,46 @@ gsap.from("#heroImage #imageHero", {
   delay: 0.5,
   duration: 0.5,
 });
+gsap.to("#heroBg #heroAll .heroPara ", {
+  stagger: 0.5,
+  opacity: 0,
+  xPercent: -100,
+  yPercent: 300,
+  rotate: 20,
+  scrollTrigger: {
+    trigger: ".section_1_box",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 30%",
+    scrub: true,
+  },
+});
+gsap.to("#heroBg #heroAll #heroImage ", {
+  stagger: 0.5,
+  opacity: 0,
+  xPercent: 100,
+  yPercent: 300,
+  rotate: -20,
+  scrollTrigger: {
+    trigger: ".section_1_box",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 30%",
+    scrub: true,
+  },
+});
 gsap.from(".section_1_box #animationBox , .section_1_box", {
   x: -400,
   y: 10,
+  rotate: 20,
   opacity: 0,
   stagger: 0.5,
   scrollTrigger: {
     trigger: ".section_1_box",
     scroller: "body",
-    // // markers: true,
+    // markers: true,
     start: "top 70%",
     end: "top 30%",
     scrub: true,
@@ -37,11 +76,12 @@ gsap.from("#provideContainer #provideImage", {
   x: -400,
   y: 10,
   opacity: 0,
+  rotate: 20,
   stagger: 0.5,
   scrollTrigger: {
     trigger: ".section_1_box",
     scroller: "body",
-    // // markers: true,
+    // markers: true,
     start: "top 40%",
     end: "top 10%",
     scrub: true,
@@ -52,10 +92,11 @@ gsap.from("#provideContainer #provideData #provideText", {
   y: 50,
   opacity: 0,
   stagger: 0.5,
+  rotate: 20,
   scrollTrigger: {
     trigger: ".section_1_box",
     scroller: "body",
-    // // markers: true,
+    // markers: true,
     start: "top 20%",
     end: "top 1%",
     scrub: true,
@@ -65,7 +106,7 @@ gsap.from("#provideContainer #provideData #provideText", {
 gsap.from("#plan #planeBox1,#plan #planeBox2,#plan #planeBox3", {
   x: -500,
   y: 50,
-  rotate: 30,
+  rotate: 50,
   opacity: 0,
   stagger: 0.5,
   scrollTrigger: {
@@ -81,11 +122,12 @@ gsap.from("#plan .planeHeading", {
   y: 10,
   scale: 0,
   opacity: 0,
+  rotate: 20,
   scrollTrigger: {
     trigger: "#plan",
     scroller: "body",
     // markers: true,
-    start: "top 40%",
+    start: "top 20%",
     end: "top 1%",
     scrub: true,
   },
